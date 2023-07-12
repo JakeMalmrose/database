473461cb7e8fece3037f59954d2166153c3dd3fa
JakeMalmrose <jmalmrose@student.neumont.edu>
2023-07-11 18:28:33 -0600

    Updated main with timer method

--- main.py
+++ main.py
@@ -1,12 +1,20 @@
+import time
 import database as db
+import os
 
 simple_path = "C:/Users/JMalmrose/Downloads/people/simple/"
 long_path = "C:/Users/JMalmrose/Downloads/people/long/"
 base_path = "C:/Users/JMalmrose/Downloads/people/"
 
 def main():
-    db.PrintPeopleDetails(base_path + "simple/")
-    db.PrintEmployees(simple_path)
+    #timing the PrintPeopleDetails function
+    print("Time to PrintPeopleDetails in simple directory: " + str(time_task(db.PrintPeopleDetails, simple_path)))
+
+def time_task(method, *args):
+    timeStart = time.perf_counter()
+    method(*args)
+    timeEnd = time.perf_counter()
+    return timeEnd - timeStart
 
 
 main()
