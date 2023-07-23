813c6cee559c666f9a8f1e548d6898aa9e11c439
JakeMalmrose <jmalmrose@student.neumont.edu>
2023-07-23 14:51:18 -0600

    Manual testing

--- main.py
+++ main.py
@@ -2,20 +2,25 @@ import time
 import database as db
 import os
 
-simple_path = "C:/Users/JMalmrose/Downloads/people/simple/"
-long_path = "C:/Users/JMalmrose/Downloads/people/long/"
-base_path = "C:/Users/JMalmrose/Downloads/people/"
-serialized_path = "C:/Users/JMalmrose/Downloads/people/serialized/"
-
 def main():
-    #timing the PrintPeopleDetails function
-    print("Time to PrintPeopleDetails in simple directory: " + str(time_task(db.PrintPeopleDetails, simple_path)))
+    client = db.ConnectToMongoDB()
+    db.PrintEmployeesMongo(client)
+    print("Adding employee")
+    db.AddEmployeeMongo(client, 3, "John", "Doe", 2019)
+    db.PrintEmployeesMongo(client)
+    print("Finding employee")
+    print(db.FindEmployeeMongo(client, 3))
+    print("Updating employee")
+    db.UpdateEmployeeMongo(client, 3, "John", "Cena", 2023)
+    db.PrintEmployeesMongo(client)
+    print("Deleting employee")
+    db.DeleteEmployeeMongo(client, 3)
+    db.PrintEmployeesMongo(client)
 
-    #serializing all employees in simple directory
-    #db.SerializeAllEmployees(simple_path)
 
-    #timing the PrintPeopleDetails function on serialized files
-    print("Time print serialized files: " + str(time_task(db.PrintPeopleDetailsSerialized, simple_path + "serialized")))
+
+
+
 
 def time_task(method, *args):
     timeStart = time.perf_counter()
